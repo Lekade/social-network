bbd632d85f0a6fe1d98631346e87f3e91688208d
Chkip <kasperovich.deonis@gmail.com>
2022-12-02 00:14:12 +0300

    lesson 92 extensions

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -18,8 +18,8 @@ let reducers = combineReducers({
     form: formReducer
 
 });
-
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 window.store = store;
 
