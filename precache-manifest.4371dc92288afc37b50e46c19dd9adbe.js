9a979c8dd0680ba41a72ce654845ac1e566c4338
Chkip <kasperovich.deonis@gmail.com>
2023-01-20 01:10:52 +0300

    Updates

---
+++ precache-manifest.4371dc92288afc37b50e46c19dd9adbe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "882c92cf9f85c9b2eba3",
+    "url": "/social-network/static/css/main.4322c7b4.chunk.css"
+  },
+  {
+    "revision": "882c92cf9f85c9b2eba3",
+    "url": "/social-network/static/js/main.882c92cf.chunk.js"
+  },
+  {
+    "revision": "98eea54d8e0bc2ca6001",
+    "url": "/social-network/static/js/1.98eea54d.chunk.js"
+  },
+  {
+    "revision": "4e855518c84b9697d74a",
+    "url": "/social-network/static/css/2.9d384262.chunk.css"
+  },
+  {
+    "revision": "4e855518c84b9697d74a",
+    "url": "/social-network/static/js/2.4e855518.chunk.js"
+  },
+  {
+    "revision": "1ce020a88c0cda5ffea2",
+    "url": "/social-network/static/css/3.8eb40ffd.chunk.css"
+  },
+  {
+    "revision": "1ce020a88c0cda5ffea2",
+    "url": "/social-network/static/js/3.1ce020a8.chunk.js"
+  },
+  {
+    "revision": "847710a9359c67db550f",
+    "url": "/social-network/static/js/4.847710a9.chunk.js"
+  },
+  {
+    "revision": "6910f837fd0ffdbd5422",
+    "url": "/social-network/static/js/runtime~main.6910f837.js"
+  },
+  {
+    "revision": "8aba6cb9edc3afc99489c9f549de0686",
+    "url": "/social-network/static/media/user.8aba6cb9.png"
+  },
+  {
+    "revision": "d2cc95d072a1cd443b5ae153f9047e99",
+    "url": "/social-network/static/media/loading.d2cc95d0.gif"
+  },
+  {
+    "revision": "37bd521b654f762574b1b3d25f39bd05",
+    "url": "/social-network/static/media/iconPhoto.37bd521b.png"
+  },
+  {
+    "revision": "6cf139a32cd287cbef2257f27547dc17",
+    "url": "/social-network/static/media/logo.6cf139a3.png"
+  },
+  {
+    "revision": "f7500c5b4c1495b6226766a84a3a334a",
+    "url": "/social-network/index.html"
+  }
+];
